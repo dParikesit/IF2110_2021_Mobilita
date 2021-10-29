23a47f4d363d79457b020d7067e6938b9ddf56ca
dParikesit <dimasparikesitt@gmail.com>
2021-10-29 21:26:44 +0700

    Added object item header

--- src/object/item/item.h
+++ src/object/item/item.h
@@ -1,3 +1,6 @@
+// File: item.h
+// Definisi object item dan fungsi yang berkaitan
+
 #ifndef ITEM_h
 #define ITEM_h
 
@@ -31,7 +34,28 @@ typedef struct Item {
 	int currentDuration;
 } Item;
 
-// Serialization Guide
+Item *getCurrentItem();
+// Get a pointer Item on the top of the bag
+
+Item *getPickUpItem();
+// Get a pointer to first item that can be picked up in current mobitaPos
+
+boolean toDoListHas(ItemType type);
+// Check if toDoList has given ItemType (for checking if there’s VIP Item or so)
+
+boolean inProgressListHas(ItemType type);
+// Check if inProgressList has given ItemType (for checking if there’s Heavy Item or so)
+
+void getItemInProgressList(ItemType type);
+// Get first item in progress list that has ItemType == type
+
+void updateItem();
+// Update perishable count item
+
+boolean isEqualItem(Item item1, Item item2);
+// Compare two items and return whether both items are
+
+/********** Serialization Guide **********/
 /* Format:
    "<timePickUp> <pickUp.letter> <dropOff.letter> <type> <maxDuration> <currentDuration>\n"
 	NB:
@@ -40,14 +64,14 @@ typedef struct Item {
 	  -> if extended, currentDuration will be read too (if not, will set the same as maxDuration)
 */
 
+void SerializeItem(Item b);
 /* Menuliskan item ke dalam stream.
    I.S. Item terdefinisi, stream terdefinisi, fileMode = WRITE.
    F.S. Menuliskan item ke dalam stream. */
-void SerializeItem(Item b);
 
+void DeserializeItem(Item *b, boolean extended);
 /* Membaca stream dan parse ke Item.
    I.S. Item sembarang, stream terdefinisi, fileMode = WRITE.
    F.S. Menginisialisasi Item dengan data di stream. */
-void DeserializeItem(Item* b, boolean extended);
 
 #endif
