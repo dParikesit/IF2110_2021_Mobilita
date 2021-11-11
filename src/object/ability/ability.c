b40b0e25a7a7663625ac573c13181f2a7279956f
M. Fahkry Malta <muhammadfahkry@gmail.com>
2021-11-11 15:17:15 +0700

    Implementasi ability

--- src/object/ability/ability.c
+++ src/object/ability/ability.c
@@ -5,10 +5,34 @@
 
 void addAbility(AbilityType ability){
     // Add ability to stats for durated ability
-    
+    switch (ability)
+    {
+    case SPEED_BOOST:
+        GSTATS.speedBoostDuration = 5;
+        break;
+
+    case RETURN_TO_SENDER:
+        GSTATS.returnToSender += 1;
+        break;
+
+    default:
+        break;
+    }
 }
 
 void applyAbility(AbilityType ability){
     // Apply effect of the selected ability
+    switch (ability)
+    {
+    case INCREASE_CAPACITY:
+        GSTATS.bagCapEff += 1;
+        break;
 
+    case RETURN_TO_SENDER:
+        GSTATS.returnToSender -= 1;
+        break;
+
+    default:
+        break;
+    }
 } 
